6d45c1bab0930110d8cc92a67186dcc23f2898a6
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 00:40:04 +0100

    showing number of jobs

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,34 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-	<h1>The Info Site</h1>
-	<nav>
-		<RouterLink to="/">Welcome</RouterLink>
-		<RouterLink to="/jobs">Jobs</RouterLink>
-		<RouterLink to="/skills">Skills</RouterLink>
-	</nav>
-	<RouterView />
+	<Suspense>
+		<div>
+			<h1>The Info Site</h1>
+			<nav>
+				<RouterLink to="/">Welcome</RouterLink>
+				<RouterLink to="/jobs">Jobs</RouterLink>
+				<RouterLink to="/skills">Skills</RouterLink>
+			</nav>
+			<RouterView />
+		</div>
+	</Suspense>
 </template>
 
 <style scoped>
 nav {
 	display: flex;
 	gap: 0.5rem;
-  background-color: #222;
-  padding: .5rem 1rem;
+	background-color: #222;
+	padding: 0.5rem 1rem;
 }
 a {
 	color: #ccc;
-  text-decoration: none;
-  font-size: 1.3rem;
+	text-decoration: none;
+	font-size: 1.3rem;
 }
 
 h1 {
-  color: rgb(206, 163, 83);
+	color: rgb(206, 163, 83);
 }
 
 a.router-link-active {
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import axios from 'axios';
+const jobs = (await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')).data;
+</script>
+
 <template>
-  <div class="jobs">
-    <p>This is an jobs page.</p>
-  </div>
+	<div class="jobs">
+		<p>There are {{ jobs.length }} jobs.</p>
+	</div>
 </template>
