a991725b7adace43fd7e8d2c99c1421f95a95aee
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-02 23:02:53 +0100

    minimized site

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <p>This is an home page.</p>
+  </div>
 </template>
