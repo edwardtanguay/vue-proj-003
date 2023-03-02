2bb4002c4af6dd6022062a09d78e09bfd094bd5e
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-02 23:58:32 +0100

    no change varaible

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 const greeting = "Welcome to this site";
-const message = 'This site contains information about available jobs.';
+let message = 'This site contains information about available jobs.';
+
+const changeMessage = () => {
+	message = 'This site also contains skills you need for IT jobs.';
+}
 </script>
 
 <template>
 	<div class="welcome">
 		<h2>{{ greeting }}</h2>
 		<p>{{ message }}</p>
+		<button @click="changeMessage">more info</button>
 	</div>
 </template>
 
