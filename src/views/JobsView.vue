6135d0539355c9b67a971bbeb192f8862f3af79a
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 00:59:59 +0100

    show jobs

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import axios from 'axios';
-const jobs = (await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')).data;
+const jobs = (
+	await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')
+).data;
 </script>
 
 <template>
 	<div class="jobs">
 		<p>There are {{ jobs.length }} jobs.</p>
+		<ul>
+			<li v-for="job in jobs">
+				<a target="_blank" :href="job.url">{{ job.title }}</a> - {{ job.company }}
+			</li>
+		</ul>
 	</div>
 </template>
+
+<style scoped>
+li {
+	color: #ccc;
+  font-size: 1.3rem;
+}
+a {
+	color: rgb(218, 218, 128);
+}
+</style>
