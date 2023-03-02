739e60f689874103f67f5ceebd5eae6f5b7f1bed
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-02 23:47:25 +0100

    scoped styles

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+const greeting = "Welcome to this site";
+const message = 'This site contains information about available jobs.';
+</script>
+
 <template>
-  <div class="welcome">
-    <p>This is an welcome page.</p>
-  </div>
+	<div class="welcome">
+		<h2>{{ greeting }}</h2>
+		<p>{{ message }}</p>
+	</div>
 </template>
+
+<style scoped>
+	h2 {
+		color: orange;
+	}
+
+	p {
+		color: #aaa;
+		font-style: italic;
+	}
+</style>
